05b619e18dbfd8c2ec73e83d643e2f8703a5804d
rainy1221 <rain102208@gmail.com>
2025-03-15 14:48:48 -0400

    e

---
+++ BoxOfCandy.java
@@ -0,0 +1,39 @@
+public class BoxOfCandy
+{
+    /** box contains at least one row and is initialized in the constructor. */
+    private Candy[][] box;
+
+    /**
+     * Moves one piece of candy in column col, if necessary and possible, so that the box
+     * element in row 0 of column col contains a piece of candy, as described in part (a).
+     * Returns false if there is no piece of candy in column col and returns true otherwise.
+     * Precondition: col is a valid column index in box.
+     */
+    public boolean moveCandyToFirstRow(int col)
+    {
+        if (box[0][col] != null) {
+            return true;
+        }
+        else {
+            for (int i = 0; i < box.length; i++) {
+                if (box[i][col] != null) {
+                    box[0][col] = box[i][col];
+                    box[i][col] = null;
+                    return true;
+                }
+            }
+            return false;
+        }
+        /* to be implemented in part (a) */ }
+
+    /**
+     * Removes from box and returns a piece of candy with flavor specified by the parameter, or
+     * returns null if no such piece is found, as described in part (b)
+     */
+    public Candy removeNextByFlavor(String flavor)
+    {
+        return null;
+        /* to be implemented in part (b) */ }
+
+    // There may be instance variables, constructors, and methods that are not shown.
+}
---
+++ Candy.java
@@ -0,0 +1,15 @@
+public class Candy {
+    String flavor;
+
+    public Candy (String flavor) {
+        this.flavor = flavor;
+    }
+
+    /** Returns a String representing the flavor of this piece of candy */
+    public String getFlavor() {
+        return flavor;
+        /* implementation not shown */ }
+
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
